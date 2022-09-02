1ff27d4536c3de207e3c5622c7fc206606a3a84a
Jin Lee <devkjlee@gmail.com>
2022-09-02 12:10:51 +1000

    new files

---
+++ BoardGame/MoveStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoardGame
+{
+    public abstract class MoveStrategy
+    {
+        protected Board board;
+        protected MoveStrategy(Board board)
+        {
+            this.board = board;
+        }
+
+        public abstract (int, int) SelectPosition();
+    }
+}
+
---
+++ BoardGame/TicTacToe/TicTacToeMoveStrategy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BoardGame
+{
+    public class TicTacToeEasyMoveStrategy : MoveStrategy
+    {
+        public TicTacToeEasyMoveStrategy(Board board) : base(board)
+        {
+        }
+
+        public override (int, int) SelectPosition()
+        {
+            List<(int, int)> locs = board.GetEmptyPositions();
+
+            // Easy level simply returns the first available move
+            if (locs.Count == 0) throw new IndexOutOfRangeException();
+
+            return locs[0];
+        }
+    }
+
+    public class TicTacToeNormalMoveStrategy : MoveStrategy
+    {
+        public TicTacToeNormalMoveStrategy(Board board) : base(board)
+        {
+        }
+
+        public override (int, int) SelectPosition()
+        {
+            List<(int, int)> locs = board.GetEmptyPositions();
+
+            if (locs.Count == 0) throw new IndexOutOfRangeException();
+
+            // Normal level returns random selected move from the avaiables
+            var random = new Random();
+            int index = random.Next(locs.Count);
+
+            return locs[index];
+        }
+    }
+}
+
